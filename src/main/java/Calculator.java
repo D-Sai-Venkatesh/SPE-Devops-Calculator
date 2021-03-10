ef9994e16b9cdf0a8dd111c1384a2512d7bc5911
vazravasu <suryasai.venkatesh@gmail.com>
2021-03-10 15:11:57 +0530

    added new build rule in pom.xml to build jar with dependencies

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -36,26 +36,29 @@ public class Calculator {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Choose operation \n"+
-                "1. Increment \n"+
-                "2. Addition  \n"+
-                "Enter your choice : ");
 
-        int op = scan.nextInt();
-        float x = 0;
-        float y = 0;
 
-        if (op == 1){
-            System.out.print("Enter operand x: ");
-            x = scan.nextFloat();
-        }
-        else if (op == 2){
-            System.out.print("Enter operands x y: ");
-            x = scan.nextFloat();
-            y = scan.nextFloat();
-        }
+            System.out.print("Choose operation \n"+
+                    "1. Increment \n"+
+                    "2. Addition  \n"+
+                    "Enter your choice : ");
 
-        Calculator mycalc = new Calculator();
-        System.out.println("Your Answer: " +  mycalc.evaluate(op, x, y));
+            int op = scan.nextInt();
+            float x = 0;
+            float y = 0;
+
+            if (op == 1){
+                System.out.print("Enter operand x: ");
+                x = scan.nextFloat();
+            }
+            else if (op == 2){
+                System.out.print("Enter operands x y: ");
+                x = scan.nextFloat();
+                y = scan.nextFloat();
+            }
+
+            Calculator mycalc = new Calculator();
+            System.out.println("Your Answer: " +  mycalc.evaluate(op, x, y));
+
     }
 }
