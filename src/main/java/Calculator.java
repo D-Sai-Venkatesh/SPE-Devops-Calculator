ff87a22509bea7f4d0980e2d44b0e121fdf01071
vazravasu <suryasai.venkatesh@gmail.com>
2021-03-13 19:16:41 +0530

    added 4 scintific operations, test cases to be added

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,6 +1,6 @@
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
+import java.lang.Math;
 import java.util.Scanner;
 
 public class Calculator {
@@ -38,6 +38,45 @@ public class Calculator {
         return temp;
     }
 
+    public float square_root(float x){
+        if(x < 0){
+            logger.error("square root function is given -ve number");
+            return -1f;
+        }
+        double temp = Math.sqrt(x);
+        logger.info("[SQRT] - " + x);
+        logger.info("[RESULT - SQRT] - " + temp);
+        return (float) temp;
+    }
+
+    public int factorial(int x){
+        if(x < 0){
+            logger.error("factorial function does not work with -ve number");
+            return -1;
+        }
+        int ans = 1;
+        for(int i = 1; i <= x; i++){
+            ans = ans * i;
+        }
+        logger.info("[FACTORIAL] - " + x);
+        logger.info("[RESULT - FACTORIAL] - " + ans);
+        return ans;
+    }
+
+    public float nat_log(float x){
+        float temp = (float) Math.log(x);
+        logger.info("[LOG] - " + x);
+        logger.info("[RESULT - LOG] - " + temp);
+        return temp;
+    }
+
+    public float power(float x, float y){
+        float temp = (float) Math.pow(x,y);
+        logger.info("[POWER] - " + x + ", " + y);
+        logger.info("[RESULT - POWER] - " + temp);
+        return temp;
+    }
+
     public float evaluate(int op, float x, float y){
         switch(op)
         {
@@ -52,6 +91,18 @@ public class Calculator {
                 break;
             case(4):
                 ans = subtract(x,y);
+                break;
+            case(5):
+                ans = square_root(x);
+                break;
+            case(6):
+                ans = factorial((int) x);
+                break;
+            case(7):
+                ans = nat_log(x);
+                break;
+            case(8):
+                ans = power(x,y);
                 break;
             default:
                 logger.error("wrong option selected");
@@ -70,17 +121,21 @@ public class Calculator {
                     "2. Addition  \n"+
                     "3. Decrement  \n"+
                     "4. Subtract  \n"+
+                    "5. Square Root  \n"+
+                    "6. Factorial  \n"+
+                    "7. Natural Log  \n"+
+                    "8. Power  \n"+
                     "Enter your choice : ");
 
             int op = scan.nextInt();
             float x = 0;
             float y = 0;
 
-            if (op == 1 || op == 3){
+            if (op == 1 || op == 3 || op == 5 || op == 6 || op == 7 ){
                 System.out.print("Enter operand x: ");
                 x = scan.nextFloat();
             }
-            else if (op == 2 || op == 4){
+            else if (op == 2 || op == 4 || op == 8 ){
                 System.out.print("Enter operands x y: ");
                 x = scan.nextFloat();
                 y = scan.nextFloat();
